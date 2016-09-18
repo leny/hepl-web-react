c7be2ada94d339926d5030cc21b839b49b27d49d
leny <info@flatland.be>
2016-09-18 21:53:15 +0200

    Organisation details

--- src/js/components/organisations/details.js
+++ src/js/components/organisations/details.js
@@ -7,24 +7,59 @@
  */
 
 import React, { Component } from "react";
-// import reqwest from "reqwest";
+import reqwest from "reqwest";
 
 export default class OrganisationDetails extends Component {
     constructor( oProps ) {
         super( oProps );
 
-        this.state = {};
+        this.state = {
+            "repos": 0,
+        };
     }
 
     componentDidMount() {
         console.log( `Fetch informations about ${ this.props.type } "${ this.props.path }" with GitHub API.` );
-        // TODO
+
+        switch ( this.props.type ) {
+            case "organisation":
+                this.fetchOrganisationInfos();
+                break;
+            case "repository":
+                // TODO
+                break;
+            default:
+                // fails silently
+                break;
+        }
+    }
+
+    fetchOrganisationInfos() {
+        reqwest( {
+            "url": `https://api.github.com/orgs/${ this.props.path }`,
+            "method": "get",
+            "type": "json",
+        } ).then( ( oOrganisationInfos ) => {
+            this.setState( {
+                "avatar": `${ oOrganisationInfos.avatar_url }&s=200`,
+                "repos": oOrganisationInfos.public_repos,
+                "url": oOrganisationInfos.html_url,
+            } );
+        } );
     }
 
     render() {
         return (
             <article>
-                <h4>{ this.props.name }</h4>
+                <a href={ this.state.url }>
+                    <h4>{ this.props.name }</h4>
+                    <figure>
+                        <img src={ this.state.avatar } />
+                    </figure>
+                    <div className="infos">
+                        <span>{ this.state.repos } dépôt{ this.state.repos > 1 && "s" }</span>
+                    </div>
+                </a>
             </article>
         );
     }
