7c440d6a4e30173e6e20b0350a5b6ea7b0334911
leny <info@flatland.be>
2016-09-20 15:04:50 +0200

    Refactor header component & styles

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -7,19 +7,22 @@
  */
 
 import React, { Component } from "react";
+import { Link } from "react-router";
 
 export default class Header extends Component {
     render() {
         return (
             <header className="header">
                 <h1 className="header__title">
-                    <abbr className="header__school" title="Haute École de la Province de Liège">HEPL</abbr>
-                    { " " }
-                    <strong className="header__bachelor">Bachelier en Techniques Graphiques</strong>
-                    { " " }
-                    <span className="header__bachelor-suffix">Orientation Techniques Infographiques</span>
-                    { " " }
-                    <em className="header__section">Section Web</em>
+                    <Link className="header__link" to="/">
+                        <abbr className="header__school" title="Haute École de la Province de Liège">HEPL</abbr>
+                        { " " }
+                        <strong className="header__bachelor">Bachelier en Techniques Graphiques</strong>
+                        { " " }
+                        <span className="header__bachelor-suffix">Orientation Techniques Infographiques</span>
+                        { " " }
+                        <em className="header__section">Section Web</em>
+                    </Link>
                 </h1>
             </header>
         );
