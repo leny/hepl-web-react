b70e9a543096fc681603c3be2e8f2f8bce66d596
leny <info@flatland.be>
2016-09-22 00:27:37 +0200

    Basic styles for side links

--- src/js/components/root.js
+++ src/js/components/root.js
@@ -42,8 +42,10 @@ export default class RootContainer extends Component {
         return (
             <div>
                 <Header />
-                { oMain }
-                <LinksList />
+                <div className="holder">
+                    { oMain }
+                    <LinksList />
+                </div>
                 <Footer />
             </div>
         );
